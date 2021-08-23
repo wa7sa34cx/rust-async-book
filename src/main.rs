375a84cc5c893ca1e9d2be1bed9dab2d99cce011
Alex Black <wa7@sa34.cx>
2021-08-23 15:10:19 +0300

    WebServer

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 #![allow(dead_code, unused_imports)]
 
 mod ch02_2_waker;
+mod ch09_webserver;
 
 use ch02_2_waker as waker;
+use ch09_webserver as webserver;
 
 fn main() {
-    waker::run();
+    // waker::run();
+    webserver::run();
 }
